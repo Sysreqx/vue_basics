5489e9caa0288015ff9df7f424bab32aabdca88b
Kaisar Yerdenbekov <sysreqx@gmail.com>
2022-09-28 20:24:42 +0600

    event listener

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <!--    {{}} interpolation    -->
+    <div>Кол-во лайков: {{likes}}</div>
+    <div>Кол-во дизлайков: {{dislikes}}</div>
+
+    <div>
+      <button v-on:click="addLike">Like</button>
+      <button v-on:click="addDislike">Dislike</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
